59a5c107b2b7588dff4c5e86b3f185aba13fcd8d
Aleksandr <sasha.syrota15@gmail.com>
2025-08-19 07:07:35 +0300

    create user model

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Airport_system
 An airport system that manage all air comunication
+![system](photo_2025-08-18_17-31-42.jpg)
--- airport_system/models.py
+++ airport_system/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.db.models import CASCADE
 
-# Create your models here.
+from user.models import User
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    closest_big_city = models.CharField(null=True)
+
+
+class Route(models.Model):
+    source = models.ForeignKey(Airport, on_delete=CASCADE)
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField()
+    user = User
